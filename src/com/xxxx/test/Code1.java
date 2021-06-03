d10e3ff8e95453cad72e6ca9713cd023c8559bab
Pine <cyym96@domain.com>
2021-06-03 21:03:12 +0800

    Initial commit

---
+++ src/com/xxxx/test/Code1.java
@@ -0,0 +1,21 @@
+package com.xxxx.test;
+
+public class Code1 {
+    public static void main(String[] args) {
+        int[] nums=new int[]{2,7,11,15};
+        int target=9;
+        twoSum(nums,target);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] num=new  int[2];
+        for(int i=0;i < (nums.length-1);i++){
+            if(nums[i]+nums[i+1]==target){
+                 num[0]=i;
+                 num[1]=i+1;
+                break;
+            }
+        }
+        return  num;
+    }
+}
